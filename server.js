07fffef81b221345d6f3dab2eed73e411b76fee4
MatheusSCA <141243967+MatheusSCA@users.noreply.github.com>
2023-09-25 17:53:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -79,29 +79,6 @@ app.post("/login",async(req, res)=>{
 app.get("/perfil",async(req, res)=>{
     res.json("entrou no perfil")
 })
-const {sign,verify} = require("jsonwebtoken")
-const createTokens = (usuario)=>{
-    const acessToken = sign({nome: usuario.nome,id:usuario.id },
-    "jwtsecret")
-
-    return acessToken
-}
-
-const validadeToken =(req,res,next)=>{
-    const acessToken = req.cookies[acess-token]
-    if(!acessToken) return res.status(404).json("algo deu errado")
-
-    try{
-        const validToken = verify(acessToken,"jwtsecret")
-        if(validToken){
-            req.autenticated = true
-            return next
-        }
-    }catch(err){
-    return res.status(404).json("erro")
-    }
-}
-module.exports ={createTokens,validadeToken}
 
 app.listen(8080,()=>{
     console.log("Rodando na porta 8080")
